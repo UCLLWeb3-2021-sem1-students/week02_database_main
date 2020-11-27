e96125d7cadbac5a3f6a6402765e68db4a83e4b1
greetjeJongen <greetje.jongen@ucll.be>
2020-11-27 08:35:07 +0100

    extra grants toegevoegd voor lectoren
    Local users van lectoren expliciet grant gegeven

--- DDL-web3_project.sql
+++ DDL-web3_project.sql
@@ -18,8 +18,12 @@ CREATE TABLE web3_project_r123456.gebruiker ( -- replace r123456 with your stude
 );
 
 GRANT ALL ON TABLE web3_project_r123456.gebruiker TO lector WITH GRANT OPTION; -- replace r123456 with your studentnr
+GRANT ALL ON TABLE web3_project_r123456.gebruiker TO local_u0015529; -- lector G. Jongen
+GRANT ALL ON TABLE web3_project_r123456.gebruiker TO local_u0082528; -- lector E. Steegmans
+GRANT ALL ON TABLE web3_project_r123456.gebruiker TO local_u0034562; -- lector K. Beheydt 
 GRANT ALL ON TABLE web3_project_r123456.gebruiker TO r123456; -- replace r123456 with your studentnr
 GRANT ALL ON TABLE web3_project_r123456.gebruiker TO local_r123456; -- replace r123456 with your studentnr
+
 
 DROP TABLE IF EXISTS web3_project_r123456.bezoek; -- replace r123456 with your studentnr
 
@@ -33,12 +37,21 @@ CREATE TABLE web3_project_r123456.bezoek ( -- replace r123456 with your studentn
                                              date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-GRANT ALL ON TABLE web3_project_r123456.bezoek TO lector; -- replace r123456 with your studentnr
+GRANT ALL ON TABLE web3_project_r123456.bezoek TO lector WITH GRANT OPTION; -- replace r123456 with your studentnr
+GRANT ALL ON TABLE web3_project_r123456.bezoek TO local_u0015529; 
+GRANT ALL ON TABLE web3_project_r123456.bezoek TO local_u0082528; 
+GRANT ALL ON TABLE web3_project_r123456.bezoek TO local_u0034562;
 GRANT ALL ON TABLE web3_project_r123456.bezoek TO r123456; -- replace r123456 with your studentnr
 GRANT ALL ON TABLE web3_project_r123456.bezoek TO local_r123456; -- replace r123456 with your studentnr
 
 GRANT ALL ON SEQUENCE web3_project_r123456.bezoek_id_seq TO lector WITH GRANT OPTION;
+GRANT ALL ON SEQUENCE web3_project_r123456.bezoek_id_seq TO local_u0015529; 
+GRANT ALL ON SEQUENCE web3_project_r123456.bezoek_id_seq TO local_u0082528; 
+GRANT ALL ON SEQUENCE web3_project_r123456.bezoek_id_seq TO local_u0034562;
 GRANT ALL ON SEQUENCE web3_project_r123456.bezoek_id_seq TO local_r123456;
 
 GRANT ALL ON SEQUENCE web3_project_r123456.gebruiker_id_seq TO lector WITH GRANT OPTION;
+GRANT ALL ON SEQUENCE web3_project_r123456.gebruiker_id_seq TO local_u0015529; 
+GRANT ALL ON SEQUENCE web3_project_r123456.gebruiker_id_seq TO local_u0082528; 
+GRANT ALL ON SEQUENCE web3_project_r123456.gebruiker_id_seq TO local_u0034562;
 GRANT ALL ON SEQUENCE web3_project_r123456.gebruiker_id_seq TO local_r123456;
